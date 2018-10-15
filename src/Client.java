83e55c8615180e95a68c0bf6cc07cd1c9701ed78
Peter Babo <pbabo17@student.aau.dk>
2018-10-15 12:02:56 +0200

    startGame method
    Started to impolement the startGame method which so far asks for a playerName and checks if the name is blank or only contains spaces. In this case, it asks for the playerName again.

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,44 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
 public class Client {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static String playerName;
+	static Scanner input = new Scanner(System.in);
+	static Socket socket;
+	static DataInputStream in;
+	static DataOutputStream out;
+	
+	//Method for starting the Client
+	public static void startClient() {
+		//Entering player name
+		while(isBlank(playerName)) {
+			System.out.print("Enter player name: ");
+			playerName = input.nextLine();
+			if(isBlank(playerName)) {
+				System.out.println("Invalid name");
+			}
+		}
+		
+		
+		//Joining the server and sending the player name
+		try {
+			socket = new Socket("localhost", 8000);
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			out.writeChars(playerName);
+		}catch(IOException ex) {
+			System.out.println("Unable to connect to server.");
+		}
 	}
 
+	//Boolean for checking if the name is blank or only contains spaces
+	private static boolean isBlank(String s) {
+		return (s == null) || (s.trim().length() == 0);
+	}
+
+	public static void main(String[] args) {
+		startClient();
+	}
 }
