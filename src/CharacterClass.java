88f0eb33fdd813e5d87d63ebb9fc5ee3cab23a07
Thevakorn <36989361+Thevakorn@users.noreply.github.com>
2018-10-16 11:00:52 +0200

    Updates for 3 classes

--- src/CharacterClass.java
+++ src/CharacterClass.java
@@ -1,4 +1,84 @@
+import java.util.Random;
+import java.util.Scanner;
 
-public class CharacterClass {
+public final class CharacterClass {
+	static Scanner scan = new Scanner(System.in);
+	static Random rand = new Random();
+	static Dice die = new Dice();
+	public static String playerName;
+	public static int playerhp;
+	public static int maxhp;
+	public static int maxmana;
+	public static int mana;
+	public static int playermeleedmg;
+	public static int xp;
+	public static int enemyhp;
+	public static int enemymeleedmg;
+	public static int Level;
+	public static int Strength;
+	public static int Dexterity;
+	public static int Intelligence;
+	public static int Constitution;
+	public static String charclass;
+	public static boolean fighting = false; // globals for player stats & enemy stats
+
+	private static void printStats() {
+		if (charclass.equals("mage")) {
+			System.out.println(playerName + "\nhp: " + playerhp + "\nmana: " + mana + "\ndamage: " + playermeleedmg
+					+ "\nxp: " + xp + "\n");
+		} else {
+			System.out.println(playerName + "\nhp: " + playerhp + "\ndamage: " + playermeleedmg + "\nxp: " + xp + "\n");
+		}
+	}
+
+	private static void printEnemyStats() {
+		System.out.println("Enemy " + "\nhp: " + enemyhp + "\ndmg: " + enemymeleedmg + "\n");
+	}
+
+	private static void buildWarrior() {
+		charclass = "warrior";
+		maxhp = 20;
+		playerhp = 20;
+		playermeleedmg = 4;
+		Strength = 10;
+		Dexterity = 6;
+		Intelligence = 5;
+		Constitution = 10;
+	}
+
+	private static void buildArcher() {
+		charclass = "archer";
+		maxhp = 14;
+		playerhp = 14;
+		playermeleedmg = 6;
+		Strength = 6;
+		Dexterity = 10;
+		Intelligence = 6;
+		Constitution = 8;
+	}
+
+	private static void buildAssassin() {
+		charclass = "assassin";
+		maxhp = 14;
+		playerhp = 14;
+		playermeleedmg = 6;
+		Strength = 8;
+		Dexterity = 10;
+		Intelligence = 6;
+		Constitution = 8;
+	}
+
+	private static void buildMage() {
+		charclass = "mage";
+		maxhp = 10;
+		playerhp = 10;
+		mana = 20;
+		maxmana = 20;
+		playermeleedmg = 2;
+		Strength = 5;
+		Dexterity = 5;
+		Intelligence = 10;
+		Constitution = 10;
+	}
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,98 @@
+import java.util.Random;
 
-public class Player {
+public final class Player {
+	
+	private final static String name;
+	private final String description;
+	private final int maxHitPoints;
+	private int hitPoints;
+	private int numPotions;
+	private final int minDamage;
+	private final int maxDamage;
+	private final Random random = new Random();
+
+	private Player(String name, int maxHitPoints, int minDamage, int maxDamage, int numPoints) {
+		this.name = name;
+		this.maxHitPoints = maxHitPoints;
+		this.minDamage = minDamage;
+		this.maxDamage = maxDamage;
+		this.numPotions = numPotions;
+		this.hitPoints = maxHitPoints;
+
+	}
+
+	public int attack() {
+		return random.nextInt(maxDamage - minDamage + 1) + minDamage;
+	}
+
+	public void defend(Player player) {
+		int attackStrength = player.attack();
+		hitPoints = (hitPoints > attackStrength) ? hitPoints - attackStrength : 0;
+		System.out.printf("  " + name + " is hit for %d HP of damage (%s)\n", attackStrength, getStatus());
+		if (hitPoints == 0) {
+			System.out.println("  " + name + " has been defeated");
+		}
+	}
+
+	public void heal() {
+		if (numPotions > 0) {
+			hitPoints = Math.min(maxHitPoints, hitPoints + 20);
+			System.out.printf("  %s drinks healing potion (%s, %d potions left)\n", name, getStatus(), --numPotions);
+		} else {
+			System.out.println("  You've exhausted your potion supply!");
+		}
+	}
+
+	public boolean isAlive() {
+		return hitPoints > 0;
+	}
+
+	public String getStatus() {
+		return "Player HP: " + hitPoints;
+	}
+
+	@Override
+	public String toString() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public static Player newInstance() {
+		return new Player("moshi moshi", 40, 6, 20, 10);
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String charclass;
+        int num = 2;
+        while(num > 1){
+        System.out.println("Enter your Name: ");
+        name = scan.nextLine();
+        System.out.println("Choose your class: ");
+        System.out.println("'w' for warrior");
+        System.out.println("'a' for archer");
+        System.out.println("'m' for mage");
+        charclass = scan.nextLine();
+        while(charclass.charAt(0) != 'w' && charclass.charAt(0) != 'a' && charclass.charAt(0) != 'm'){
+            System.out.println("'w' for warrior");
+            System.out.println("'a' for archer");
+            System.out.println("'m' for mage");
+            charclass = scan.nextLine();
+        }
+        if(charclass.charAt(0) == 'w'){
+            buildWarrior();
+        }
+        if(charclass.charAt(0) == 'a'){
+            buildArcher();
+        }
+        if(charclass.charAt(0) == 'm'){
+            buildMage();
+        }
+        if(charclass.charAt(0)== 's') {
+        	buildAssassin();
+        }
 
 	}
 
