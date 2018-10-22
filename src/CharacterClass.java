d66f493c2ff956fde4620b5decadf13e6ad9ca6d
Thevakorn <36989361+Thevakorn@users.noreply.github.com>
2018-10-22 23:28:28 +0200

    Small changes

--- src/CharacterClass.java
+++ src/CharacterClass.java
@@ -11,6 +11,8 @@ public final class CharacterClass {
 	public static int maxmana;
 	public static int mana;
 	public static int playermeleedmg;
+	public static int playerrangedmg;
+	public static int playerfireball;
 	public static int Strength;
 	public static int Dexterity;
 	public static int Intelligence;
@@ -18,12 +20,12 @@ public final class CharacterClass {
 	public static String charclass;
 	public static boolean fighting = false; // globals for player stats & enemy stats
 
-	private static void printStats() {
+	static void printStats() {
 		if (charclass.equals("mage")) {
-			System.out.println(playerName + "\nhp: " + playerhp + "\nmana: " + mana + "\ndamage: " + playermeleedmg
-					+ "\nxp: " + "\n");
+			System.out.println(Client.playerName + "\nhp: " + playerhp + "\nmana: " + mana + "\ndamage: " + playermeleedmg
+					 + "\n");
 		} else {
-			System.out.println(playerName + "\nhp: " + playerhp + "\ndamage: " + playermeleedmg + "\nxp: " + "\n");
+			System.out.println(Client.playerName + "\nhp: " + playerhp + "\ndamage: " + playermeleedmg + "\n");
 		}
 	}
 
@@ -31,46 +33,72 @@ public final class CharacterClass {
 		System.out.println("Enemy " + "\nhp: " + "\ndmg: " + "\n");
 	}
 
+<<<<<<< HEAD
 	private static void buildWarrior() {
+		charclass = "Warrior";
+		maxhp = 18;
+		playerhp = 18;
+=======
+	static void buildWarrior() {
 		charclass = "warrior";
 		maxhp = 20;
 		playerhp = 20;
+>>>>>>> 2708c494baddc7b922645594747b71171b90cd54
 		playermeleedmg = 4;
 		Strength = 10;
-		Dexterity = 6;
+		Dexterity = 5;
 		Intelligence = 5;
 		Constitution = 10;
 	}
 
+<<<<<<< HEAD
 	private static void buildRanger() {
-		charclass = "archer";
+		charclass = "Ranger";
+=======
+	static void buildRanger() {
+		charclass = "ranger";
+>>>>>>> 2708c494baddc7b922645594747b71171b90cd54
 		maxhp = 14;
 		playerhp = 14;
-		playermeleedmg = 6;
+		playermeleedmg = 2;
+		playerrangedmg = 4;
 		Strength = 6;
 		Dexterity = 10;
 		Intelligence = 6;
 		Constitution = 8;
 	}
 
+<<<<<<< HEAD
 	private static void buildAssassin() {
+		charclass = "Assassin";
+=======
+	static void buildAssassin() {
 		charclass = "assassin";
+>>>>>>> 2708c494baddc7b922645594747b71171b90cd54
 		maxhp = 14;
 		playerhp = 14;
-		playermeleedmg = 6;
+		playermeleedmg = 4;
 		Strength = 8;
 		Dexterity = 10;
 		Intelligence = 6;
 		Constitution = 8;
 	}
 
+<<<<<<< HEAD
 	private static void buildMage() {
+		charclass = "Mage";
+		maxhp = 12;
+		playerhp = 12;
+=======
+	static void buildMage() {
 		charclass = "mage";
 		maxhp = 10;
 		playerhp = 10;
+>>>>>>> 2708c494baddc7b922645594747b71171b90cd54
 		mana = 20;
 		maxmana = 20;
 		playermeleedmg = 2;
+		playerfireball = 6; //fireball should cost 8 mana for each use
 		Strength = 5;
 		Dexterity = 5;
 		Intelligence = 10;
